793229c9bd69204530b6a0db53ff910068df326d
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-02-11 20:10:48 +0100

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from './utils/errors';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(
   }),
 );
 app.use(json());
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
   console.log('Listening on port http://localhost:3001');
