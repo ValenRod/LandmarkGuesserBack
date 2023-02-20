0ea83e751962742ca71f8baa34e596a19f7e5713
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-02-20 18:21:28 +0100

    Added RoundNumber enum and RoundEntity interface.

---
+++ types/round/round-entity.ts
@@ -0,0 +1,17 @@
+export enum RoundNumber {
+  First = 1,
+  Second = 2,
+  Third = 3,
+  Fourth = 4,
+  Fifth = 5,
+}
+
+export interface RoundEntity {
+  id: string;
+  roundNumber: RoundNumber;
+  landmarkId: string;
+  playerGuessLat: number | null;
+  playerGuessLng: number | null;
+  distance: number | null;
+  points: number | null;
+}
