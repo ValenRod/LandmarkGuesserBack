61e8a34bbb9e966af2913dd18aa49a8ecacd8484
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-03-02 21:58:14 +0100

    Added endpoint in gameRouter for updating current round data and total points of specific game.

--- routers/game.router.ts
+++ routers/game.router.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 import {landmarkDraw} from '../utils/landmarkDraw';
 import {LandmarkRecord} from '../records/landmark.record';
 import {
+  Coordinates,
+  GameUpdateRequestBody,
   NewGameEntity,
   NewRoundEntity,
   RoundNumber,
@@ -9,6 +11,8 @@ import {
 } from '../types';
 import {RoundRecord} from '../records/round-record';
 import {GameRecord} from '../records/game.record';
+import haversineDistance from 'haversine-distance';
+import {pointsCalc} from '../utils/points';
 
 const createNewRound = async (
   currentRound: RoundNumber,
@@ -47,4 +51,39 @@ export const gameRouter = Router()
     const newGame = new GameRecord(newGameObj);
     await newGame.insert();
     res.json(newGame.id);
+  })
+  .put('/:id', async (req, res) => {
+    const game = new GameRecord(await GameRecord.getOne(req.params.id));
+    const rounds = Object.values(game.rounds);
+    const currentRound = game.currentRound;
+    const totalPoints = game.totalPoints;
+    const reqBody: GameUpdateRequestBody = req.body;
+
+    const round = new RoundRecord(
+      await RoundRecord.getOne(rounds[currentRound - 1]),
+    );
+    const landmark = new LandmarkRecord(
+      await LandmarkRecord.getOne(round.landmarkId),
+    );
+
+    const {playerGuessCoordinates} = reqBody;
+    const landmarkCoordinates: Coordinates = {
+      lat: landmark.lat,
+      lng: landmark.lng,
+    };
+
+    const distance = Math.round(
+      haversineDistance(playerGuessCoordinates, landmarkCoordinates),
+    );
+    const points = pointsCalc(distance);
+
+    round.playerGuessLat = playerGuessCoordinates.lat;
+    round.playerGuessLng = playerGuessCoordinates.lng;
+    round.distance = distance;
+    round.points = points;
+    game.totalPoints = totalPoints + round.points;
+
+    await round.update();
+    await game.update();
+    res.end();
   });
