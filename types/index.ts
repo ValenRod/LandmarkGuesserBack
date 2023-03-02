2f8f3eb6b82abcab07fb33a588dfef950361610d
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-03-02 21:37:20 +0100

    Added Coordinates and GameUpdateRequestBody interfaces.

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,5 @@
 export * from './landmark';
 export * from './round';
 export * from './game';
+export * from './coordinates';
+export * from './request';
---
+++ types/request/request.ts
@@ -0,0 +1,5 @@
+import {Coordinates} from '../coordinates';
+
+export interface GameUpdateRequestBody {
+  playerGuessCoordinates: Coordinates;
+}
