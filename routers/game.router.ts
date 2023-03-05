bdff68b6d123af8bab60a5fc37f80d5cf4969aee
PawełWasilczyk <pawel.wasilczyk98@gmail.com>
2023-03-05 14:01:17 +0100

    Moving round creation and getting round params utils to round.ts file in utils directory.

--- routers/game.router.ts
+++ routers/game.router.ts
@@ -5,9 +5,7 @@ import {
   Coordinates,
   GameParams,
   GameUpdateRequestBody,
-  LandmarkParams,
   NewGameEntity,
-  NewRoundEntity,
   RoundNumber,
   RoundParams,
   RoundsOfGameEntity,
@@ -16,45 +14,7 @@ import {RoundRecord} from '../records/round-record';
 import {GameRecord} from '../records/game.record';
 import haversineDistance from 'haversine-distance';
 import {pointsCalc} from '../utils/points';
-
-const createNewRound = async (
-  roundNumber: RoundNumber,
-  landmarkId: string,
-): Promise<string> => {
-  const newRoundObj: NewRoundEntity = {
-    roundNumber,
-    landmarkId,
-  };
-  const round = new RoundRecord(newRoundObj);
-  return await round.insert();
-};
-
-const getRoundParams = async (roundId: string): Promise<RoundParams> => {
-  const roundData = await RoundRecord.getOne(roundId);
-  const landmarkData = new LandmarkRecord(
-    await LandmarkRecord.getOne(roundData.landmarkId),
-  );
-
-  let landmark: LandmarkParams;
-  const {url, lat, lng} = landmarkData;
-
-  if (roundData.points !== null) {
-    landmark = {url, lat, lng};
-  } else {
-    landmark = {
-      url,
-      lat: null,
-      lng: null,
-    };
-  }
-
-  const round = (({id, landmarkId, ...roundData}) => roundData)(roundData);
-
-  return {
-    ...round,
-    landmark,
-  };
-};
+import {createNewRound, getRoundParams} from '../utils/round';
 
 export const gameRouter = Router()
   .get('/', (req, res) => res.redirect('/'))
